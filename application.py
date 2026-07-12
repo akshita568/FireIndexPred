5fc3b400b3561c934d4c2878dc246eb9bc910216
Akshita Sharma <sakshita229@gmail.com>
2026-07-12 19:36:34 +0530

    deploy

--- application.py
+++ application.py
@@ -46,8 +46,5 @@ def predict_datapoint():
     else:
         return render_template('home.html')
 
-import os
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(debug=True)
